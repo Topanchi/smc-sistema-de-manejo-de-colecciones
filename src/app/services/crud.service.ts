0b474a040bca26973bd44e8f0441d26815dc6413
Francisco Fuentes <franciscofs2000@gmail.com>
2020-12-10 11:24:04 -0300

    WIP:  servicios y lista. Determinar realtime DB o Firesotre

--- src/app/services/crud.service.ts
+++ src/app/services/crud.service.ts
@@ -1,9 +1,63 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from '@angular/fire/database';  // Firebase modules for Database, Data list and Single object
+
+export interface Llavero { 
+  $key: string;
+  nombre: string; 
+  material: string;
+  numero: number;
+  notas: string;
+  pais: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
 
-  constructor() { }
+  llaverosRef: AngularFireList<any>;    // Reference to Llaveros data list, its an Observable
+  llaveroRef: AngularFireObject<any>;   // Reference to Lllavero object, its an Observable too
+  
+  // Inject AngularFireDatabase Dependency in Constructor
+  constructor(private db: AngularFireDatabase) { }
+
+  // Create Student
+  AddStudent(llavero: Llavero) {
+    this.llaverosRef.push({
+      nombre: llavero.nombre,
+      material: llavero.material,
+      numero: llavero.numero,
+      pais: llavero.pais,
+      comentarios: llavero.notas
+    })
+  }
+
+  // Fetch Single Student Object
+  GetStudent(id: string) {
+    this.llaveroRef = this.db.object('llaveros/' + id);
+    return this.llaveroRef;
+  }
+  // Fetch Students List
+  GetStudentsList() {
+    this.llaverosRef = this.db.list('llaveros');
+    return this.llaverosRef;
+  } 
+
+  // Update Student Object
+  UpdateStudent(llavero: Llavero) {
+    this.llaveroRef.update({
+      nombre: llavero.nombre,
+      material: llavero.material,
+      numero: llavero.numero,
+      pais: llavero.pais,
+      comentarios: llavero.notas
+    })
+  }  
+  // Delete Student Object
+  DeleteStudent(id: string) { 
+    this.llaveroRef = this.db.object('llaveros/'+id);
+    this.llaveroRef.remove();
+  }
+
+
 }
