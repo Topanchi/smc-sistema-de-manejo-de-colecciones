3828398a4bf44241d6dd62c93859edf7f0f7dc81
Francisco Fuentes <franciscofs2000@gmail.com>
2020-12-11 11:15:35 -0300

    WIP: add ver-llavero componenet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { AregarLlaveroComponent } from './aregar-llavero/aregar-llavero.component';
 import { EditarLlaveroComponent } from './editar-llavero/editar-llavero.component';
 import { ListarLlaveroComponent } from './listar-llavero/listar-llavero.component';
+import { VerLlaveroComponent } from './ver-llavero/ver-llavero.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/agregar-llavero', pathMatch: 'full' },
   { path: 'agregar-llavero', component: AregarLlaveroComponent },
   { path: 'listar-llaveros', component: ListarLlaveroComponent },
-  { path: 'editar-llavero/:id', component: EditarLlaveroComponent }
+  { path: 'editar-llavero/:id', component: EditarLlaveroComponent },
+  { path: 'ver-llavero/:id', component: VerLlaveroComponent }
 ];
 
 @NgModule({
---
+++ src/app/ver-llavero/ver-llavero.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ver-llavero',
+  templateUrl: './ver-llavero.component.html',
+  styleUrls: ['./ver-llavero.component.css']
+})
+export class VerLlaveroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
